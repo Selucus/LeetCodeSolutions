37939d317d3dffbb7a4f91587a30e375610c62c2
Ben Chapman <bchapman141@gmail.com>
2024-09-26 22:27:04 +0100

    solved with optimal solution

--- 729 - My Calendar 1.py
+++ 729 - My Calendar 1.py
@@ -1,4 +1,4 @@
-class MyCalendar:
+"""class MyCalendar:
 
     def __init__(self):
         self.bookings = []
@@ -20,4 +20,26 @@ class MyCalendar:
 myCalendar = MyCalendar();
 print(myCalendar.book(10, 20))
 print(myCalendar.book(15, 25)) 
-print(myCalendar.book(20, 30))      
+print(myCalendar.book(20, 30))      """
+import bisect
+class MyCalendar:
+
+    def __init__(self):
+        self.bookings = [(-1,-1),(float('inf'),float('inf'))]
+
+    def book(self, start: int, end: int) -> bool:
+        index = bisect.bisect_left(self.bookings,(start,end))
+
+        if start < self.bookings[index-1][1]:
+            return False
+        if end > self.bookings[index][0]:
+            return False
+
+        self.bookings.insert(index,(start,end))
+        return True        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
+
